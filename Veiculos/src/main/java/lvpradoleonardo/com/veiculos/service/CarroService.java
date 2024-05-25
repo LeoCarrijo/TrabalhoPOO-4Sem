c6dcbb30f56d77c48b411e50c7f4e3cd5ca670e9
LeoCarrijo <lh83712@gmail.com>
2024-05-25 20:30:53 -0300

    :sparkles: feat: Criação de Método para Conversão de List de Entities para DTOs e para Consulta de Todos Carros

--- Veiculos/src/main/java/lvpradoleonardo/com/veiculos/service/CarroService.java
+++ Veiculos/src/main/java/lvpradoleonardo/com/veiculos/service/CarroService.java
@@ -6,12 +6,27 @@ import lvpradoleonardo.com.veiculos.model.repository.CarroRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class CarroService {
     @Autowired
     CarroRepository injecao;
     public CarroDTO insere(CarroDTO carroDTO) {
         return converteParaDTO(injecao.save(converteParaEntity(carroDTO)));
+    }
+
+    public List<CarroDTO> consultarTodos() {
+        return converteEntitiesParaDTOs(injecao.findAll());
+    }
+
+    public List<CarroDTO> converteEntitiesParaDTOs(List<CarroEntity> carroEntities) {
+        List<CarroDTO> carroDTOs = new ArrayList<CarroDTO>();
+        for (CarroEntity carroEntity : carroEntities) {
+            carroDTOs.add(converteParaDTO(carroEntity));
+        }
+        return carroDTOs;
     }
 
     public CarroDTO converteParaDTO(CarroEntity carroEntity) {
