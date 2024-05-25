0252b83fe95af4c1210d324fe66b950d25a75011
LeoCarrijo <lh83712@gmail.com>
2024-05-25 19:58:32 -0300

    :sparkles: feat: Criação do Arquivo 'ClienteEntity'

---
+++ Veiculos/src/main/java/lvpradoleonardo/com/veiculos/CarroEntity.java
@@ -0,0 +1,67 @@
+package lvpradoleonardo.com.veiculos;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="carros")
+public class CarroEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    @Column(name = "marca")
+    private String marca;
+    @Column(name = "modelo")
+    private String modelo;
+    @Column(name = "ano")
+    private int ano;
+    @Column(name = "valor")
+    private float valor;
+
+    public CarroEntity(int id, String marca, String modelo, int ano, float valor) {
+        this.id = id;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.ano = ano;
+        this.valor = valor;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public float getValor() {
+        return valor;
+    }
+
+    public void setValor(float valor) {
+        this.valor = valor;
+    }
+}
