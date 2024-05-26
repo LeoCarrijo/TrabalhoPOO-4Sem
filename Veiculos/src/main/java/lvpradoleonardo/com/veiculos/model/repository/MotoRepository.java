e75c9370d60688728d91fa42d1b7c8494161ce3c
LeoCarrijo <lh83712@gmail.com>
2024-05-26 09:57:12 -0300

    :sparkles: feat: Criação do Arquivo 'MotoService'

--- Veiculos/src/main/java/lvpradoleonardo/com/veiculos/model/repository/MotoRepository.java
+++ Veiculos/src/main/java/lvpradoleonardo/com/veiculos/model/repository/MotoRepository.java
@@ -4,5 +4,5 @@ import lvpradoleonardo.com.veiculos.model.entity.MotoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MotoRepository extends JpaRepository<MotoEntity, Long> {
-    
+
 }
---
+++ Veiculos/src/main/java/lvpradoleonardo/com/veiculos/service/MotoService.java
@@ -0,0 +1,15 @@
+package lvpradoleonardo.com.veiculos.service;
+
+import lvpradoleonardo.com.veiculos.model.entity.MotoEntity;
+import lvpradoleonardo.com.veiculos.model.repository.MotoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MotoService {
+    @Autowired
+    MotoRepository injecao;
+    public MotoEntity insere(MotoEntity moto) {
+        return injecao.save(moto);
+    }
+}
