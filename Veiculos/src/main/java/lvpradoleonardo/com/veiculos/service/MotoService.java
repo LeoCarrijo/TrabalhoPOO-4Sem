4b3294ec476ca979078d368ea572a76f5f62dc57
LeoCarrijo <lh83712@gmail.com>
2024-05-26 10:17:59 -0300

    :sparkles: feat: Criação dos Métodos de Conversão de DTO para Entity e vice-versa

--- Veiculos/src/main/java/lvpradoleonardo/com/veiculos/service/MotoService.java
+++ Veiculos/src/main/java/lvpradoleonardo/com/veiculos/service/MotoService.java
@@ -1,5 +1,6 @@
 package lvpradoleonardo.com.veiculos.service;
 
+import lvpradoleonardo.com.veiculos.model.dto.MotoDTO;
 import lvpradoleonardo.com.veiculos.model.entity.MotoEntity;
 import lvpradoleonardo.com.veiculos.model.repository.MotoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,5 +12,23 @@ public class MotoService {
     MotoRepository injecao;
     public MotoEntity inserirMoto(MotoEntity moto) {
         return injecao.save(moto);
+    }
+
+    public MotoEntity converteParaEntity(MotoDTO motoDTO) {
+        return new MotoEntity(motoDTO.getId(),
+                motoDTO.getMarca(),
+                motoDTO.getModelo(),
+                motoDTO.getAno(),
+                motoDTO.getValor(),
+                motoDTO.getCc());
+    }
+
+    public MotoDTO converteParaDTO(MotoEntity motoEntity) {
+        return new MotoDTO(motoEntity.getId(),
+                motoEntity.getMarca(),
+                motoEntity.getModelo(),
+                motoEntity.getAno(),
+                motoEntity.getValor(),
+                motoEntity.getCc());
     }
 }
