cedb276090f56a2dfcc2890fadfb0d2a60a481c5
LeoCarrijo <lh83712@gmail.com>
2024-06-06 20:58:24 -0300

    :sparkles: feat: Criação do 'BarcoService'

--- Veiculos/src/main/java/lvpradoleonardo/com/veiculos/model/repository/BarcoRepository.java
+++ Veiculos/src/main/java/lvpradoleonardo/com/veiculos/model/repository/BarcoRepository.java
@@ -1,6 +1,7 @@
 package lvpradoleonardo.com.veiculos.model.repository;
 
+import lvpradoleonardo.com.veiculos.model.entity.BarcoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BarcoRepository extends JpaRepository<BarcoRepository, Long> {
+public interface BarcoRepository extends JpaRepository<BarcoEntity, Long> {
 }
