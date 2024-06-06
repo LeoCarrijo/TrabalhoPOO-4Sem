b6fdf71242ff968c1212153ca6506357a7a77f88
LeoCarrijo <lh83712@gmail.com>
2024-06-06 20:34:46 -0300

    :sparkles: feat: Criação do 'BarcoRepository'

---
+++ Veiculos/src/main/java/lvpradoleonardo/com/veiculos/model/repository/BarcoRepository.java
@@ -0,0 +1,6 @@
+package lvpradoleonardo.com.veiculos.model.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BarcoRepository extends JpaRepository<BarcoRepository, Long> {
+}
